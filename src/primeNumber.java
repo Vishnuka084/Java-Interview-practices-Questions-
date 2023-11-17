d1a785f73e4aec92dccc805904721b0b77218259
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-18 01:09:16 +0530

    check Prime Number

---
+++ src/primeNumber.java
@@ -0,0 +1,32 @@
+package src;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/18/2023
+ * @project : Interview
+ */
+public class primeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(19));
+        System.out.println(isPrime(49));
+    }
+
+    private static boolean isPrime(int n) {
+
+        if (n == 0 || n == 1){
+            return false;
+        }
+        if (n == 2 ){
+            return false;
+        }
+        for (int i = 2; i <=n/2  ; i++) {
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
