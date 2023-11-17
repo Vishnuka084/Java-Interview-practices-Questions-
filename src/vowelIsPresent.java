2c766a07fae43287cf7affb560abee74e0a2bb37
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-18 00:56:03 +0530

    check if a vowel is present in a string.

---
+++ src/vowelIsPresent.java
@@ -0,0 +1,23 @@
+package src;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/18/2023
+ * @project : Interview
+ */
+public class vowelIsPresent {
+
+    public static void main(String[] args) {
+        System.out.println(stringVowels("Hellos"));
+        System.out.println(stringVowels("Tv"));
+
+    }
+
+    private static boolean stringVowels(String input) {
+
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+
+}
