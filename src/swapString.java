c304066d17f558721cda17c5cd71cc4d41664603
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-18 00:29:53 +0530

    swap without third variable

---
+++ src/swapString.java
@@ -0,0 +1,23 @@
+package src;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/18/2023
+ * @project : Interview
+ */
+public class swapString {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(" a  : " +a+ "\n b : " +b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(" a  : " +a+ "\n b : " +b);
+    }
+}
