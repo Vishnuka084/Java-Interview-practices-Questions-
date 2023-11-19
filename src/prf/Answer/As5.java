4218ec9c29d72bb19924ba104f91c5ea172a87f9
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-19 22:31:53 +0530

    Updated Content

---
+++ src/prf/Answer/As5.java
@@ -0,0 +1,20 @@
+package src.prf.Answer;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 11/19/2023
+ * @project : Interview
+ */
+public class As5 {
+    public static void main(String[] args) {
+        oneAnswer();
+    }
+
+    private static void oneAnswer() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
